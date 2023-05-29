61f09d6614ecf12fd36efb04f92197f4f15aa0d7
rasoul <8raha6094@gmail.com>
2023-05-30 01:54:17 +0430

    initial api call for login and sighnup done

---
+++ config.h
@@ -0,0 +1,7 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define API_ADRESS "https://test.com"
+
+#endif // CONFIG_H
--- setupmain.cpp
+++ setupmain.cpp
@@ -1,5 +1,13 @@
 #include "setupmain.h"
 #include "ui_setupmain.h"
+#include <QtCore>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QDebug>
+#include <config.h>
+#include <QMessageBox>
 
 SetupMain::SetupMain(QWidget *parent) :
     QWidget(parent),
@@ -36,5 +44,81 @@ void SetupMain::on_btn_signup_cancel_clicked()
 void SetupMain::on_btn_login_cancel_clicked()
 {
      ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void SetupMain::on_btn_login_login_clicked()
+{
+     if(ui->input_login_username->text().isEmpty() || ui->input_login_password->text().isEmpty()){
+         QMessageBox::warning(this, tr("Warning"), tr("Please enter username and password!"));
+         return;
+     }
+
+     QString username= ui->input_login_username->text();
+     QString password= ui->input_login_password->text();
+
+     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+     QUrl url(QString(API_ADRESS)+"/login?username="+username+"&password="+password);
+     QNetworkRequest request(url);
+
+     //*
+
+
+     QNetworkReply* reply = manager->get(request);
+
+     // connect signals and slots to handle the response
+     connect(reply, &QNetworkReply::finished, [=]() mutable {
+         if (reply->error() != QNetworkReply::NoError) {
+             qDebug()<<"Login Error: " << reply->errorString();
+         } else {
+             QByteArray response = reply->readAll();
+             qDebug()<<response;
+
+              qDebug()<<"Logged in successfully with token: ";
+         }
+         reply->deleteLater();
+
+     });
+
+}
+
+
+void SetupMain::on_btn_signup_signup_clicked()
+{
+     if(ui->input_login_username->text().isEmpty() || ui->input_login_password->text().isEmpty()){
+         QMessageBox::warning(this, tr("Warning"), tr("Please enter username and password!"));
+         return;
+     }
+
+
+     QString  username =ui->input_signup_username->text();
+     QString  password =ui->input_signup_password->text();
+     QString  firstname =ui->input_firstname->text();
+     QString  lastname =ui->input_lastname->text();
+
+     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+
+     QUrl url(QString(API_ADRESS)+"/sighnup?username="+username+"password="+password+"firstname="+firstname+"lastname"+lastname);
+     QNetworkRequest request(url);
+
+
+
+     QNetworkReply* reply = manager->get(request);
+
+     QObject::connect(reply, &QNetworkReply::finished, [=]() mutable {
+         if (reply->error() != QNetworkReply::NoError) {
+             qDebug()<<"Signup Error: " << reply->errorString();
+         } else {
+             QByteArray response = reply->readAll();
+             QString message = QString(response);
+
+             if (message == "up signed successfully") {
+                 qDebug()<<"Signed up successfully.";
+             } else {
+                 qDebug()<<"Signup Error: " + message;
+             }
+         }
+         reply->deleteLater();
+     });
 }
 
